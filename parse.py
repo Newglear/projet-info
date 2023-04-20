a9d5fe2be250b48042bf72b5ddc656ded4098a50
Joelimgu <joelimgu@gmail.com>
2023-04-20 12:11:30 +0200

    parse.py

---
+++ parse.py
@@ -0,0 +1,29 @@
+import re
+import sys
+
+
+def main():
+    labels = {} # label: line_num
+    infile = sys.argv[1]
+    with open("out_file.asm", "w") as out_f:
+        jmp_pattern = re.compile("JM. __.*__")
+        label_pattern = re.compile("__.*__")
+        with open(infile, "r") as f:
+            for index, line in enumerate(f.readlines()):
+                if label_pattern.match(line):
+                    labels[label_pattern.search(line)[0]] = index
+
+        with open(infile, "r") as f:
+            for line in f.readlines():
+                if jmp_pattern.match(line):
+                    label = label_pattern.search(line)[0]
+                    index = labels[label]
+                    line = line.replace(label, str(index))
+                elif label_pattern.match(line):
+                    labels[label_pattern.search(line)[0]] = index
+                    line = "PASS\n"
+                out_f.write(line)
+
+
+if __name__ == "__main__":
+    main()
